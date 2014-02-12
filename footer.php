23e6ec9540c87f87d90651e81bf8c4c100604bd5
David Beauchamp <dbeauchamp@makermedia.com>
2014-02-11 17:34:48 -0800

    Adding the images to the img/makercamp folder

--- footer.php
+++ footer.php
@@ -108,7 +108,7 @@
 						<!-- END span3 about-us -->
 						</div>
 						<div class="span3 subscribe hidden-phone">
-							<div class="sprite sprite-arrow-footer"></div>
+							<div class="sprite sprite-arrow-footer visible-desktop"></div>
 							<a href="https://readerservices.makezine.com/mk/subscribe.aspx?PC=MK&amp;PK=M**NEWB" target="_blank">
 								<img src="http://i1.wp.com/cdn.makezine.com/make/covers/MAKE_V37_high.jpg?resize=128%2C181" alt="MAKE Magazine Drones" width="128" height="181" id="mag-cover" class="pull-right">
 							</a>
