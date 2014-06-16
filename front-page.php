0a18689f7ee77c573118e5e7435cde55d4b6ecff
Bill Olson <bolson@makermedia.com>
2014-06-16 13:50:15 -0700

    Home page sessions

--- front-page.php
+++ front-page.php
@@ -1,25 +1,25 @@
 <?php
-/**
- * Home Page template
- *
- * @package    maker-camp
- * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
- *
- */
-get_header(); ?>
+	/**
+	 * Home Page template
+	 *
+	 * @package    maker-camp
+	 * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
+	 *
+	 */
+	get_header(); ?>
 <div class="home blog makercamp makercamp-new ">
-<div class="container-fluid">
-	<span class="visible-desktop skyships"></span>
-	<div class="main-header">
+	<div class="container-fluid">
+		<span class="visible-desktop skyships"></span>
+		<div class="main-header">
 			<div class="container">
 				<div class="row-fluid">
 					<div class="hidden-desktop text-center">
 						<div class="span12">
 							<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/logo.png' ); ?>" />
-
 							<div class="tagline">
 								<h1>A <strong>FREE</strong>
-								summer camp for building, tinkering and exploring. Online and in your neighborhood!</h1>
+									summer camp for building, tinkering and exploring. Online and in your neighborhood!
+								</h1>
 							</div>
 						</div>
 					</div>
@@ -30,110 +30,96 @@ get_header(); ?>
 						<div class="span6" style="">
 							<div class="tagline">
 								<h1>A <strong>FREE</strong>
-								summer camp for building, tinkering and exploring. Online and in your neighborhood!</h1>
+									summer camp for building, tinkering and exploring. Online and in your neighborhood!
+								</h1>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	<section class="white-bg intro">
-		<div class="container">
-			<div class="row-fluid visible-desktop">
-				<div class="span8">
-					<h1>Welcome to Maker Camp!</h1>
-					<p>Join young inventors and artists from around the world on <strong>Google+</strong> to make awesome projects, go on epic virtual "field trips," and meet the world's coolest makers. Maker Camp inspires <strong>kids 13-18</strong> to embrace their inner maker, get their hands dirty, fix some things, break some things, and have a lot of fun doing it.</p>
-					<div class="ytube_intro">
-						<iframe width="100%" height="100%" src="http://www.youtube.com/embed/Lcd0Pv2eCgk" frameborder="0" allowfullscreen></iframe>
+		<section class="white-bg intro">
+			<div class="container">
+				<div class="row-fluid visible-desktop">
+					<div class="span8">
+						<h1>Welcome to Maker Camp!</h1>
+						<p>Join young inventors and artists from around the world on <strong>Google+</strong> to make awesome projects, go on epic virtual "field trips," and meet the world's coolest makers. Maker Camp inspires <strong>kids 13-18</strong> to embrace their inner maker, get their hands dirty, fix some things, break some things, and have a lot of fun doing it.</p>
+						<div class="ytube_intro">
+							<iframe width="100%" height="100%" src="http://www.youtube.com/embed/Lcd0Pv2eCgk" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+					<div class="span4 text-center visible-desktop">
+						<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/sidepic-kids.png' ); ?>" style="margin-bottom:8px;" /><br>
+						<span style="font-size:14px;color:#666;">Make awesome projects!</span>
+						<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/sidepic.png' ); ?>"  style="margin-bottom:8px;margin-top:30px;" /><br>
+						<span style="font-size:14px;color:#666;">Meet the world's best makers!</span>
 					</div>
 				</div>
-				<div class="span4 text-center visible-desktop">
-
-
-
-					<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/sidepic-kids.png' ); ?>" style="margin-bottom:8px;" /><br>
-					<span style="font-size:14px;color:#666;">Make awesome projects!</span>
-
-
-
-					<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/sidepic.png' ); ?>"  style="margin-bottom:8px;margin-top:30px;" /><br>
-					<span style="font-size:14px;color:#666;">Meet the world's best makers!</span>
-				</div>
-			</div>
-			<div class="row-fluid hidden-desktop">
-				<div class="span12">
-					<h1>Welcome to Maker Camp!</h1>
-					<p class="tagline">Join young inventors and artists from around the world on <strong>Google+</strong> to make awesome projects, go on epic virtual "field trips," and meet the world's coolest makers. Maker Camp inspires <strong>kids 13-18</strong> to embrace their inner maker, get their hands dirty, fix some things, break some things, and have a lot of fun doing it.</p>
-					<div class="ytube_intro">
-						<iframe width="100%" height="100%" src="http://www.youtube.com/embed/Lcd0Pv2eCgk" frameborder="0" allowfullscreen></iframe>
+				<div class="row-fluid hidden-desktop">
+					<div class="span12">
+						<h1>Welcome to Maker Camp!</h1>
+						<p class="tagline">Join young inventors and artists from around the world on <strong>Google+</strong> to make awesome projects, go on epic virtual "field trips," and meet the world's coolest makers. Maker Camp inspires <strong>kids 13-18</strong> to embrace their inner maker, get their hands dirty, fix some things, break some things, and have a lot of fun doing it.</p>
+						<div class="ytube_intro">
+							<iframe width="100%" height="100%" src="http://www.youtube.com/embed/Lcd0Pv2eCgk" frameborder="0" allowfullscreen></iframe>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-	<section class="beige-bg hidden-phone" class="past-projects">
-		<div class="container">
-			<div class="row-fluid">
-				<div class="span12">
-					<div class="row-fluid">
-						<div class="span9">
-							<h4 style="line-height:1.4">What will we Make? A look at past projects</h4>
+		</section>
+		<section class="beige-bg hidden-phone" class="past-projects">
+			<div class="container">
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="row-fluid">
+							<div class="span9">
+								<h4 style="line-height:1.4">What will we Make? A look at past projects</h4>
+							</div>
+							<div class="span3">
+								<h4 style="line-height:1.4">
+									<a class="pull-right" style="color:#005d8f !important" href="http://staging.makercamp.com/?page_id=9">View More Projects</a>
+								</h4>
+							</div>
 						</div>
-						<div class="span3">
-							<h4 style="line-height:1.4">
-								<a class="pull-right" style="color:#005d8f !important" href="http://vip.dev/maker-camp/?page_id=6">View More Projects</a>
-							</h4>
-						</div>
-					</div>
-					<div class="row-fluid">
-						<div class="span3 clearfix">
-							<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">
+						<div class="row-fluid">
+							<div class="span3 clearfix">
+								<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">
 								<img alt="Casting Your Imagination" src="http://i0.wp.com/makezineblog.files.wordpress.com/2013/08/keychain1.jpg?resize=270%2C174" />
-							</a>
-							<h6 style="color:#005d8f !important">
-								<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">Casting Your Imagination</a>
-							</h6>
-							<p>Description about this project goes here with a couple sentences about what the project is and what it does.</p>
-
+								</a>
+								<h6 style="color:#005d8f !important">
+									<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">Casting Your Imagination</a>
+								</h6>
+								<p>Project: <a style="color:#005d8f !important;" href="http://makezine.com/projects/custom-robot-keychains-with-easy-moldmaking/">Robot keychain</a> &amp; <a style="color:#005d8f !important;" href="http://makezine.com/projects/unihorn-bike-helmet-with-littlebits/">Unihorn Bike Helmet</a></p>
+							</div>
+							<div class="span3">
+								<a href="https://plus.google.com/109780686446922422512/posts/XyyTQnotsxZ">
+								<img alt="Playing with Plastic" src="http://i0.wp.com/makezineblog.files.wordpress.com/2013/08/keychain1.jpg?resize=270%2C174" />
+								</a>
+								<h6 style="color:#005d8f !important">
+									<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">Playing with Plastic  &amp; Another Slice of Raspberry Pi</a>
+								</h6>
+								<p> Project: <a style="color:#005d8f !important;" href="http://makezine.com/projects/how-to-make-your-own-plastic-vacuum-former/">Peanut Butter Jar Vacuum Former</a></p>
+							</div>
+							<div class="span3">
+								<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">
+								<img alt="Cyborg Yourself " src="http://i0.wp.com/makezineblog.files.wordpress.com/2013/08/keychain1.jpg?resize=270%2C174" />
+								</a>
+								<h6 style="color:#005d8f !important">
+									<a href="https://plus.google.com/u/0/109780686446922422512/posts/CcBkj3jq5c8">Cyborg Yourself </a>
+								</h6>
+								<p> Project: <a style="color:#005d8f !important;" href="http://makezine.com/projects/cyborg-makeup-effect/">Cyborg Makeup</a></p>
+							</div>
+							<div class="span3 last">
+								<a href="https://plus.google.com/+MAKE/posts/SVCwD2JiQ6A">
+								<img alt="South African Young Makers" src="http://i0.wp.com/makezineblog.files.wordpress.com/2013/08/keychain1.jpg?resize=270%2C174" />
+								</a>
+								<h6 style="color:#005d8f !important;"><a href="https://plus.google.com/+MAKE/posts/SVCwD2JiQ6A">South African Young Makers</a> &amp; <a href="https://plus.google.com/109780686446922422512/posts/cYeTznXvHW4">Props and Weathering Effects</a></h6>
+								<p>                  Project: <a style="color:#005d8f !important;" href="http://makezine.com/projects/simple-paint-weathering-in-3-easy-steps/">Weathering Effects</a></p>
+							</div>
 						</div>
-						<div class="span3">
-							<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">
-								<img alt="Casting Your Imagination" src="http://i0.wp.com/makezineblog.files.wordpress.com/2013/08/keychain1.jpg?resize=270%2C174" />
-							</a>
-							<h6 style="color:#005d8f !important">
-								<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">Casting Your Imagination</a>
-							</h6>
-							<p>Description about this project goes here with a couple sentences about what the project is and what it does.</p>
-
-						</div>
-						<div class="span3">
-							<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">
-								<img alt="Casting Your Imagination" src="http://i0.wp.com/makezineblog.files.wordpress.com/2013/08/keychain1.jpg?resize=270%2C174" />
-							</a>
-							<h6 style="color:#005d8f !important">
-								<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">Casting Your Imagination</a>
-							</h6>
-							<p>Description about this project goes here with a couple sentences about what the project is and what it does.</p>
-
-						</div>
-						<div class="span3 last">
-							<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">
-								<img alt="Casting Your Imagination" src="http://i0.wp.com/makezineblog.files.wordpress.com/2013/08/keychain1.jpg?resize=270%2C174" />
-							</a>
-							<h6 style="color:#005d8f !important">
-								<a href="https://plus.google.com/u/0/109780686446922422512/posts/BbxwHANTcap">Casting Your Imagination</a>
-							</h6>
-							<p>Description about this project goes here with a couple sentences about what the project is and what it does.</p>
-
-						</div>
-
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
+	</div>
 </div>
-</div>
-
-
 <?php get_footer(); ?>
