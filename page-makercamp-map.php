b605a4e57ef02fc01495ceca20f246e38a7e5dad
Bill Olson <bolson@makermedia.com>
2014-06-19 15:28:06 -0700

    Affiliate Map updates

--- page-makercamp-map.php
+++ page-makercamp-map.php
@@ -5,11 +5,12 @@
  *
  * @package    makeblog
  * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
+ * @author     Cole Geissinger <cgeissinger@makermedia.com>
  *
  */
 ?>
 
-<?php get_header( ); ?>
+<?php get_header( 'makercamp' ); ?>
 	<?php
 		$get_addresses = get_post_meta( get_the_ID(), 'makercamp-maps-data', false );
 		$addresses = json_decode( str_replace( '&quot;', '"', $get_addresses[0] ), true );
@@ -30,9 +31,12 @@
 						<table class="table table-striped map-list">
 							<thead class="map-list-header">
 								<tr>
-									<th style="width:110px;">State, City</th>
-									<th style="width:20px;">Country</th>
+									<th style="width:55px;">Country</th>
+									<th style="width:75px;">State</th>
+									<th style="width:140px;">City</th>
 									<th style="width:228px;">Organization</th>
+									<th style="width:81px;">Website</th>
+									<th style="width:25px;">&nbsp;</th>
 								</tr>
 							</thead>
 							<tbody>
@@ -40,17 +44,16 @@
 									if ( ! empty( $addresses ) && is_array( $addresses ) ) :
 										foreach ( $addresses as $address ) : ?>
 											<tr>
-												<td><?php echo esc_attr( $address['Work State, Work City'] ); ?></td>
 												<td><?php echo esc_attr( $address['Work Country'] ); ?></td>
-												<td><a href="<?php echo esc_url( $address['Website'] ); ?>">
-														<?php echo esc_url( $address['Website'] ); ?><?php echo esc_attr( $address['Company'] ); ?>
-													</a>
-												</td>
-												<!-- <td>
+												<td><?php echo esc_attr( $address['Work State'] ); ?></td>
+												<td><?php echo esc_attr( $address['Work City'] ); ?></td>
+												<td><?php echo esc_attr( $address['Company'] ); ?></td>
+												<td><a href="<?php echo esc_url( $address['Website'] ); ?>"><?php echo esc_url( $address['Website'] ); ?></a></td>
+												<td>
 													<?php if ( isset( $addressss['Google Link'] ) ) : ?>
 														<a href="<?php echo esc_url( $address['google-plus'] ); ?>"><img src="http://makezineblog.files.wordpress.com/2013/06/google-plus.png"></a>
 													<?php endif; ?>
-												</td> -->
+												</td>
 											</tr>
 										<?php endforeach;
 									endif;
