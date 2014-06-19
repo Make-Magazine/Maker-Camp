76509d0a54567e10ea033e24157b911fb5948e2e
Bill Olson <bolson@makermedia.com>
2014-06-19 15:21:49 -0700

    Style and affiliate map page updates

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
  * @package    makeblog
  * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
  * @author     Cole Geissinger <cgeissinger@makermedia.com>
- * 
+ *
  */
 ?>
 
@@ -16,7 +16,7 @@
 		<title><?php wp_title(); ?></title>
 		<meta name="google-site-verification" content="tjgq9UGR8WCMZI_40j_B5wda_oVYqKyFtQW547LzMgQ" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+
 		<!-- Le styles -->
 		<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
 
@@ -24,7 +24,7 @@
 		<!--[if lt IE 9]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-		
+
 		<?php wp_head( 'makercamp' ); ?>
 
 	</head>
@@ -33,22 +33,24 @@
 			<div class="navbar main-nav navbar-fixed-top">
 				<div class="navbar-inner">
 					<div class="container">
-						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</a>
-						<a href="<?php echo esc_url( home_url() ); ?>" class="brand visible-phone visible-tablet">Maker Camp</a>
-						<div class="nav-collapse in collapse">
-							<?php
-								// All Navigational items are controlled in Appearance > Menus : Maker Camp Nav
-								wp_nav_menu( array(
-									'theme_location' => 'mc-header-menu',
-									'container' => '',
-									'menu_class' => 'nav primary-nav nav-inline',
-									'walker' => new Bootstrap_Walker_Nav_Menu(),
-								) );
-							?>
+						<div class="row-fluid">
+							<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</a>
+							<a href="<?php echo esc_url( home_url() ); ?>" class="brand visible-phone visible-tablet">Maker Camp</a>
+							<div class="nav-collapse in collapse">
+								<?php
+									// All Navigational items are controlled in Appearance > Menus : Maker Camp Nav
+									wp_nav_menu( array(
+										'theme_location' => 'mc-header-menu',
+										'container' => '',
+										'menu_class' => 'nav primary-nav nav-inline',
+										'walker' => new Bootstrap_Walker_Nav_Menu(),
+									) );
+								?>
+							</div>
 						</div>
 					</div>
 				</div>
