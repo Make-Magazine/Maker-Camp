7140525660f099c7bf621a1cc5214fb5d91e76ef
Bill Olson <bolson@makermedia.com>
2014-07-03 17:17:35 -0700

    Update for maps page list

--- page-makercamp-map.php
+++ page-makercamp-map.php
@@ -10,101 +10,103 @@
  */
 ?>
 
-<?php get_header( 'makercamp' ); ?>
-  <?php
-    $get_addresses = get_post_meta( get_the_ID(), 'makercamp-maps-data', false );
-    $addresses = json_decode( str_replace( '&quot;', '"', $get_addresses[0] ), true );
-  ?>
-  <script>
-    jQuery(document).ready(function( $ ) {
-      $( '.map-list' ).tablesorter();
-    });
-  </script>
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php get_header(); ?>
+	<?php
+		$get_addresses = get_post_meta( get_the_ID(), 'makercamp-maps-data', false );
+		$addresses = json_decode( str_replace( '&quot;', '"', $get_addresses[0] ), true );
+	?>
+	<script>
+		jQuery(document).ready(function( $ ) {
+			$( '.map-list' ).tablesorter();
+		});
+	</script>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <div class="makercamp-new ">
-    <div class="main-header">
-      <div class="container">
-        <div class="row-fluid" >
-          <div class="hidden-desktop text-center">
-            <div class="span12">
-              <img  style="margin:30px auto" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/mc-logo-2014.gif' ); ?>" />
-              <div class="tagline" style="margin-top:25px;">
-                <h1>A <strong>FREE</strong>
-                  summer camp for building, tinkering and exploring. Online and in your neighborhood!
-                </h1>
-                                <h1><strong>July 7th-August 15th, 2014</strong><br />Daily at 11am Pacific Time<h1>
+		<div class="main-header">
+			<div class="container">
+				<div class="row-fluid" >
+					<div class="hidden-desktop text-center">
+						<div class="span12">
+							<img  style="margin:30px auto" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/mc-logo-2014.gif' ); ?>" />
+							<div class="tagline" style="margin-top:25px;">
+								<h1>A <strong>FREE</strong>
+									summer camp for building, tinkering and exploring. Online and in your neighborhood!
+								</h1>
+																<h1><strong>July 7th-August 15th, 2014</strong><br />Daily at 11am Pacific Time<h1>
 
-              </div>
+							</div>
 
-            </div>
-          </div>
-          <div class="visible-desktop">
-            <div class="span6">
-              <img style="margin:30px auto" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/mc-logo-2014.gif' ); ?>" />
-            </div>
-            <div class="span6" style="">
-              <div class="tagline" style="margin-top:25px;">
-                <h1>A <strong>FREE</strong>
-                  summer camp for building, tinkering and exploring. Online and in your neighborhood!
-                </h1>
-                                <h1><strong>July 7th-August 15th, 2014</strong><br />Daily at 11am Pacific Time<h1>
+						</div>
+					</div>
+					<div class="visible-desktop">
+						<div class="span6">
+							<img style="margin:30px auto" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/mc-logo-2014.gif' ); ?>" />
+						</div>
+						<div class="span6" style="">
+							<div class="tagline" style="margin-top:25px;">
+								<h1>A <strong>FREE</strong>
+									summer camp for building, tinkering and exploring. Online and in your neighborhood!
+								</h1>
+																<h1><strong>July 7th-August 15th, 2014</strong><br />Daily at 11am Pacific Time<h1>
 
-              </div>
+							</div>
 
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  <section class="white-bg map-page">
-    <div class="container ">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<section class="white-bg map-page">
+		<div class="container ">
 
-      <div class="row-fluid">
+			<div class="row-fluid">
 
-        <div class="span12">
+				<div class="span12">
 
-              <?php the_content(); ?>
+							<?php the_content(); ?>
 
-        </div>
-      </div>
-      <div class="row">
-        <div class="span12">
-          <table class="table table-striped map-list">
-            <thead class="map-list-header">
-              <tr>
-                <th style="width:55px;">Country</th>
-                <th style="width:75px;">State</th>
-                <th style="width:140px;">City</th>
-                <th style="width:228px;">Organization</th>
-                <th style="width:81px;">Website</th>
-                <th style="width:25px;">&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-                if ( ! empty( $addresses ) && is_array( $addresses ) ) :
-                  foreach ( $addresses as $address ) : ?>
-                    <tr>
-                      <td><?php echo esc_attr( $address['Work Country'] ); ?></td>
-                      <td><?php echo esc_attr( $address['Work State'] ); ?></td>
-                      <td><?php echo esc_attr( $address['Work City'] ); ?></td>
-                      <td><?php echo esc_attr( $address['Company'] ); ?></td>
-                      <td><a href="<?php echo esc_url( $address['Website'] ); ?>"><?php echo esc_url( $address['Website'] ); ?></a></td>
-                      <td>
-                        <?php if ( isset( $addressss['Google Link'] ) ) : ?>
-                          <a href="<?php echo esc_url( $address['google-plus'] ); ?>"><img src="http://makezineblog.files.wordpress.com/2013/06/google-plus.png"></a>
-                        <?php endif; ?>
-                      </td>
-                    </tr>
-                  <?php endforeach;
-                endif;
-              ?>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </section>
-  <?php endwhile; endif; ?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="span12">
+					<table class="table table-striped map-list">
+						<thead class="map-list-header">
+							<tr>
+								<th style="width:55px;">Country</th>
+								<th style="width:50px;">State</th>
+								<th style="width:140px;">City</th>
+								<th style="width:258px;">Organization</th>
+								<th style="width:81px;">Accepting</th>
+								<th style="width:81px;">Pre-Register</th>
+								<th style="width:25px;">&nbsp;</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								if ( ! empty( $addresses ) && is_array( $addresses ) ) :
+									foreach ( $addresses as $address ) : ?>
+										<tr>
+											<td><?php echo esc_attr( $address['Work Country'] ); ?></td>
+											<td><?php echo esc_attr( $address['Work State'] ); ?></td>
+											<td><?php echo esc_attr( $address['Work City'] ); ?></td>
+											<td><a href="<?php echo esc_url( $address['Website'] ); ?>"><?php echo esc_attr( $address['Company'] ); ?></a></td>
+											<td><?php echo esc_attr( $address['Accepting'] ); ?></td>
+											<td><?php echo esc_attr( $address['Register'] ); ?></td>
+											<td>
+												<?php if ( isset( $addressss['Google Link'] ) ) : ?>
+														<a href="http://<?php echo esc_url( $address['Google Link'] ); ?>"><img src="http://makezineblog.files.wordpress.com/2013/06/google-plus.png"></a>
+												<?php endif; ?>
+											</td>
+										</tr>
+									<?php endforeach;
+								endif;
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php endwhile; endif; ?>
 
-<?php get_footer( 'makercamp' ); ?>
+<?php get_footer(); ?>
