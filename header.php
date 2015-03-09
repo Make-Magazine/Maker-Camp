e153f8a3bead214292dc2c358af4598a7d5054cd
Bill Olson <bolson@makermedia.com>
2015-03-09 16:52:42 -0700

    GA Update

--- header.php
+++ header.php
@@ -24,7 +24,20 @@
 		<!--[if lt IE 9]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-		
+
+		<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			  ga('create', 'UA-51157-25', 'auto');
+			  ga('require', 'displayfeatures');
+			  ga('send', 'pageview', {
+			  'page': location.pathname + location.search  + location.hash
+			        });
+		</script>
+
 		<script>
 			var _prum = [['id', '53fcea2fabe53d341d4ae0eb'],
 			            ['mark', 'firstbyte', (new Date()).getTime()]];
--- js/common.js
+++ js/common.js
@@ -1,17 +1,6 @@
 // This file contains common JavaScript that is loaded into every page.
 // We want to register and enqueue these scripts with WordPress.
 
-// Load Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-51157-25']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 jQuery( document ).ready( function( $ ) {
     // Sadly the Facebook Comment Box does not allow us to change the positioning
     $( '.comment-list' ).appendTo( '#comments' );
